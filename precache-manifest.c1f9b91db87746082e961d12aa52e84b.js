6cf2577c9de3a1fb8956cf80c6ec8a50643332f8
prabhath akula <64606345+prabhath07@users.noreply.github.com>
2020-07-18 21:14:37 +0530

    Updates

---
+++ precache-manifest.c1f9b91db87746082e961d12aa52e84b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bf240ce5092bc76292359cb5c0d19a3",
+    "url": "/facedetect/index.html"
+  },
+  {
+    "revision": "db7e87e7f6cae5e9b32d",
+    "url": "/facedetect/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "25893bdf29f0ec734b84",
+    "url": "/facedetect/static/css/main.10fdee1d.chunk.css"
+  },
+  {
+    "revision": "db7e87e7f6cae5e9b32d",
+    "url": "/facedetect/static/js/2.2c1ce023.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/facedetect/static/js/2.2c1ce023.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25893bdf29f0ec734b84",
+    "url": "/facedetect/static/js/main.9bc8b0dc.chunk.js"
+  },
+  {
+    "revision": "c1012a071d59b478631c",
+    "url": "/facedetect/static/js/runtime-main.f7589b46.js"
+  },
+  {
+    "revision": "0ca6bee8fb698be881fbd47a199d5425",
+    "url": "/facedetect/static/media/Logo.0ca6bee8.png"
+  }
+]);
